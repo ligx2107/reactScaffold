7bf3e2a6e80db7b3844591288963fbc086b7de31
liguangxin <liguangxin2107@126.com>
2021-12-19 21:43:54 +0800

    redux mini version

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@
 import React from 'react';
 //引入ReactDOM
 import ReactDOM from 'react-dom';
-//引入React-router-dom
-import {BrowserRouter} from 'react-router-dom';
 //引入组件
 import App from './App';
 
 //渲染App组件到页面
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>, 
+        <App />,
     document.getElementById('root')
 );
